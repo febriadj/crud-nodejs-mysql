950aeddb3e24626737dfcd494a99cda417b51359
i'mfebri <73212374+febriadj@users.noreply.github.com>
2020-12-25 00:33:34 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple CRUD Nodejs MySQL
+Membuat aplikasi crud sederhana untuk table data-data mahasiswa menggunakan nodejs + expressjs dan database mysql
---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const dotenv = require('dotenv');
+const mysql = require('mysql');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/public')));
+dotenv.config({ path: './.env' });
+
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE
+});
+
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+app.get('/table', (req, res, next) => {
+  let sql = `SELECT * FROM mahasiswa`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.render('table', {
+      datas: result
+    });
+  });
+});
+
+app.get('/table/post', (req, res, next) => {
+  res.render('postdata');
+});
+
+app.post('/table/post', (req, res, render) => {
+  console.log(req.body);
+  let {nim, nama, jurusan} = req.body;
+  let sql = `INSERT INTO mahasiswa VALUES(0, '${nim}', '${nama}', '${jurusan}')`;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.redirect('/table');
+  })
+})
+
+app.use((req, res, next) => {
+  res.status(404).render('404');
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('mysql terkoneksi');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
